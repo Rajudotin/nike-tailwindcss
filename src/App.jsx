a9aeb1ec9e9280eacbc43a835b0c35f17369d8ae
Rajudotin <balavenkatarajusingampalli@gmail.com>
2026-02-06 10:55:10 +0530

    update footer

--- src/App.jsx
+++ src/App.jsx
@@ -29,13 +29,13 @@ const App = () => {
       <section className='padding-x sm:py-32 py-16 w-full'>
         <Subscribe/>
       </section>
-      <section className='bg-balck padding-x padding-t pb-8'>
+      <section className='bg-black padding-x padding-t pb-8'>
         <Footer/>
       </section>
     </main>
 
   )
-}
+};
 
 export default App;
 
--- src/sections/footer.jsx
+++ src/sections/footer.jsx
@@ -16,7 +16,7 @@ const Footer = () => {
               className='m-0'
             />
           </a>
-          <p className='mt-6 text-base leading-7 font-montserrat text-slate-gray sm:max-w-sm'>
+          <p className='mt-6 text-base leading-7 font-montserrat text-white-400 sm:max-w-sm'>
             Get shoes ready for the new term at your nearest Nike store. Find
             Your perfect Size In Store. Get Rewards
           </p>
@@ -35,13 +35,13 @@ const Footer = () => {
         <div className='flex flex-1 justify-between lg:gap-10 gap-20 flex-wrap'>
           {footerLinks.map((section) => (
             <div key={section.title}>
-              <h4 className='font-montserrat text-2xl leading-normal font-medium mb-6 text-slate-gray'>
+              <h4 className='font-montserrat text-2xl leading-normal font-medium mb-6 text-white'>
                 {section.title}
               </h4>
               <ul>
                 {section.links.map((link) => (
                   <li
-                    className='mt-3 font-montserrat text-base leading-normal text-slate-gray'
+                    className='mt-3 font-montserrat text-base leading-normal text-white-400 hover:text-slate-gray'
                     key={link.name}
                   >
                     <a href={link.link}>{link.name}</a>
@@ -53,7 +53,7 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className='flex justify-between text-slate-gray mt-24 max-sm:flex-col max-sm:items-center'>
+      <div className='flex justify-between text-white-400 mt-24 max-sm:flex-col max-sm:items-center'>
         <div className='flex flex-1 justify-start items-center gap-2 font-montserrat cursor-pointer'>
           <img
             src={copyrightSign}
